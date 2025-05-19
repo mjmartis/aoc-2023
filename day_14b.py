5bbe58a862f4603ddaf445c4d7293346b1e70518
Michael Martis <michaeljmartis@gmail.com>
2025-05-19 20:39:24 +1000

    Switched 14b to 'per cycle' caching

--- day_14b.py
+++ day_14b.py
@@ -2,7 +2,7 @@ import sys
 
 from utils import gT, i_range
 
-STEPS = 1000000000 * 4
+CYCLES = 1000000000
 
 def rotate_cw(g):
   return [c[::-1] for c in gT(g)]
@@ -19,38 +19,36 @@ def tilt_row(row):
       rock_count += 1
   return tilted
 
-def tilt_grid(g, d):
-  for _ in range(d):
-    g = rotate_cw(g)
-  g = [tilt_row(r) for r in g]
-  for _ in range((4-d) % 4):
-    g = rotate_cw(g)
-  return g
-
 def calc_load(g):
   return sum(sum(i+1 for i, c in enumerate(r) if c == 'O') for r in g)
 
 def solve(instream):
   g_in = [[c for c in l[:-1]] for l in instream.readlines()]
 
-  # Iterate tilts until we see a repeat.
+  # Iterate cycles until we see a repeat.
   have_seen_g = set()
   seen_gs = []
   g = rotate_cw(g_in)
   while True:
-    g = tilt_grid(g, len(have_seen_g) % 4)
+    # Complete a full cycle before caching.
+    #
+    # If the number of unique board states is coprime with 4, this would require 4x more
+    # iterations than caching a tilt at a time. However for my problem input, caching by
+    # cycles proves faster.
+    for _ in range(4):
+      g = rotate_cw([tilt_row(r) for r in g])
+
     if (key := tuple(map(tuple, g))) not in have_seen_g:
       have_seen_g.add(key)
       seen_gs.append(g)
     else:
       break
   
-  # Find the start of the cycle and the offset into the cycle that matches the target
-  # step.
-  cycle_start_i = seen_gs.index(g)
-  cycle_offset_i = (STEPS - 1 - cycle_start_i) % (len(seen_gs) - cycle_start_i)
+  # Find the start of the loop and offset into the loop that matches the target step.
+  loop_start_i = seen_gs.index(g)
+  loop_offset_i = (CYCLES-1-loop_start_i) % (len(seen_gs)-loop_start_i)
 
-  print(calc_load(seen_gs[cycle_start_i + cycle_offset_i]))
+  print(calc_load(seen_gs[loop_start_i+loop_offset_i]))
 
 if __name__ == '__main__':
   solve(sys.stdin)
