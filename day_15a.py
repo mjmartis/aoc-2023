e8cb369f8c75663542676679ccd96afed846a165
Michael Martis <michaeljmartis@gmail.com>
2025-05-20 19:20:18 +1000

    Forgot

---
+++ day_15a.py
@@ -0,0 +1,13 @@
+import sys
+
+def solve(instream):
+  total = 0
+  for s in ''.join(l[:-1] for l in instream.readlines()).split(','):
+    case_total = 0
+    for c in s:
+      case_total = (case_total + ord(c)) * 17 % 256
+    total += case_total
+  print(total)
+
+if __name__ == '__main__':
+  solve(sys.stdin)
